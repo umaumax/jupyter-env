124e6393a40f889b518a680fbce9340762902344
umaumax <umauma7083@gmail.com>
2024-03-23 17:14:37 +0900

    Refactor

--- scripts/conv-chrome-trace.py
+++ scripts/conv-chrome-trace.py
@@ -5,38 +5,6 @@ import argparse
 import uuid
 
 
-def normalize_chrome_trace(input_chrome_trace):
-    output_normalized_chrome_trace = input_chrome_trace.copy()
-    normalized_trace_events = []
-    for i, event in enumerate(input_chrome_trace['traceEvents']):
-        if 'ph' not in event:
-            print(
-                '[warning] There is no "ph" field at ".traceEvents[{}]"'.format(i))
-            continue
-        if event['ph'] == 'X':
-            trace_event_b = event.copy()
-            trace_event_b['ph'] = 'B'
-            del trace_event_b['dur']
-            trace_event_e = event.copy()
-            trace_event_e['ph'] = 'E'
-            trace_event_e['ts'] = event['ts'] + event['dur']
-            del trace_event_e['dur']
-            normalized_trace_events.append(trace_event_b)
-            normalized_trace_events.append(trace_event_e)
-        elif event['ph'] in ['B', 'E']:
-            normalized_trace_events.append(event.copy())
-        else:
-            print(
-                '[warning] There is no implementation of ".traceEvents[{}].ph": "{}"'.format(
-                    i, event['ph']))
-            continue
-    normalized_trace_events = sorted(
-        normalized_trace_events,
-        key=lambda x: x['ts'])
-    output_normalized_chrome_trace['traceEvents'] = normalized_trace_events
-    return output_normalized_chrome_trace
-
-
 def convert_json_to_chrome_trace(
         input_file, output_chrome_trace_file):
     with open(input_file, 'r') as f:
